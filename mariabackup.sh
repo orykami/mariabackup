504ac7b0dbc099f2f395e3c14667054f1078f269
Benoit JACQUOT <88.jacquot.benoit@gmail.com>
2019-04-25 14:39:38 +0200

    Bash synthax fixes + cosmetics

--- mariabackup.sh
+++ mariabackup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+##
+# [Mariabackup agent]
+# @author Orykami <88.jacquot.benoit@gmail.com>
+##
+
 MYSQL_USER=""
 MYSQL_PASSWORD=""
 MYSQL_HOST=localhost
@@ -7,14 +12,16 @@ MYSQL_PORT=3306
 MYSQL="$(which mysql)"
 MARIABACKUP="$(which mariabackup)"
 MYSQLADMIN="$(which mysqladmin)"
+FULL_SNAPSHOT_CYCLE=604800
 ARGS=""
 
+# Load script configuration from usual paths
 if [[ -f config/mariabackup.conf ]]
 then
   . config/mariabackup.conf
 elif [[ -f ~/.mariabackup.conf ]]
 then
-  . ~/.mariabackup.conf
+  . ~/config/mariabackup.conf
 elif [[ -f /etc/mariabackup.conf ]]
 then
   . /etc/mariabackup.conf
@@ -29,6 +36,7 @@ DATE="$(date +"%d-%m-%Y")"
 TIME="$(date +"%H-%M-%S")"
 
 
+# Create recursive directory specified
 mkdir_writable_directory() {
   if [[ $# -ne 1 ]] || [[ -z $1 ]]
   then
@@ -46,10 +54,9 @@ mkdir_writable_directory() {
 ##
 # Main ()
 ##
-
-# Display startup log
 echo -e "[`date +"%d/%m/%Y %H:%M:%S"`] ${MYSQL_HOST} [info] Start mariabackup.sh (Galera/MariaDB backup agent)"
 
+# Create FULL_SNAPSHOT directory
 mkdir_writable_directory ${FULL_SNAPSHOT_DIR}
 if [[ $? -ne 0 ]]
 then
@@ -57,6 +64,7 @@ then
   exit 1
 fi
 
+# Create INCR_SNAPSHOT directory
 mkdir_writable_directory ${INCR_SNAPSHOT_DIR}
 if [[ $? -ne 0 ]]
 then
@@ -64,48 +72,54 @@ then
   exit 1
 fi
 
+# Ensure that mariabackup is able to connect to MariaDB server
 if ! `echo 'exit' | ${MYSQL} -s ${USEROPTIONS}`
 then
   echo -e "[`date +"%d/%m/%Y %H:%M:%S"`] ${MYSQL_HOST} [error] Can't connect to MariaDB instance (user/password missmatch ?)";
   exit 1
 fi
 
-# Find latest backup directory
+# Find latest full backup snapshot as reference for later
 LATEST_FULL_SNAPSHOT=`find ${FULL_SNAPSHOT_DIR} -mindepth 1 -maxdepth 1 -type d -printf "%P\n" | sort -nr | head -1`
 LATEST_FULL_SNAPSHOT_AGE=`stat -c %Y ${FULL_SNAPSHOT_DIR}/${LATEST_FULL_SNAPSHOT}`
 
-if [[ "$LATEST_FULL_SNAPSHOT" -a `expr ${LATEST_FULL_SNAPSHOT_AGE} + ${FULL_SNAPSHOT_CYCLE} + 5` -ge ${START_TIME} ]]
+# If latest full snapshot is expired, we should create a new full snapshost
+if [ "$LATEST_FULL_SNAPSHOT" -a `expr ${LATEST_FULL_SNAPSHOT_AGE} + ${FULL_SNAPSHOT_CYCLE} + 5` -ge ${START_TIME} ]
 then
-
   echo -e "[`date +"%d/%m/%Y %H:%M:%S"`] ${MYSQL_HOST} [info] Create new incremental snapshot"
+  # Create incremental snapshot repository if needed
   mkdir_writable_directory ${INCR_SNAPSHOT_DIR}/${LATEST_FULL_SNAPSHOT}
   if [[ $? -ne 0 ]]; then
     echo -e "[`date +"%d/%m/%Y %H:%M:%S"`] ${MYSQL_HOST} [error] '${INCR_SNAPSHOT_DIR}/${LATEST_FULL_SNAPSHOT}' does not exist or is not writable."
     exit 1
   fi
 
-  LATEST_INCR_SNAPSHOT=`find ${INCR_SNAPSHOT_DIR/$LATEST_FULL_SNAPSHOT} -mindepth 1  -maxdepth 1 -type d | sort -nr | head -1`
-  if [[ ! ${LATEST_INCR_SNAPSHOT} ]]; then
-    INCR_BASE_DIR=${FULL_SNAPSHOT_DIR/$LATEST_FULL_SNAPSHOT}
+  # Find latest incremental snapshot as reference for later
+  LATEST_INCR_SNAPSHOT=`find ${INCR_SNAPSHOT_DIR}/${LATEST_FULL_SNAPSHOT} -mindepth 1  -maxdepth 1 -type d | sort -nr | head -1`
+  if [[ -z ${LATEST_INCR_SNAPSHOT} ]]
+  then
+    INCR_BASE_DIR=${FULL_SNAPSHOT_DIR}/${LATEST_FULL_SNAPSHOT}
   else
     INCR_BASE_DIR=${LATEST_INCR_SNAPSHOT}
   fi
 
-  NEXT_SNAPSHOT_DIR=${INCR_SNAPSHOT_DIR/$LATEST_FULL_SNAPSHOT}/`date +%F_%H-%M-%S`
+  # Create next incremental snapshot directory
+  NEXT_SNAPSHOT_DIR=${INCR_SNAPSHOT_DIR}/${LATEST_FULL_SNAPSHOT}/`date +%F_%H-%M-%S`
   mkdir -p ${NEXT_SNAPSHOT_DIR}
 
-  # Create incremental Backup
+  # Start next incremental snapshot with mariabackup agent
   ${MARIABACKUP} \
     --backup ${USEROPTIONS} ${ARGS} \
     --extra-lsndir=${NEXT_SNAPSHOT_DIR} \
     --incremental-basedir=${INCR_BASE_DIR} \
     --stream=xbstream | gzip > ${NEXT_SNAPSHOT_DIR}/backup.stream.gz
 else
+  # Create next full snapshot directory
   echo -e "[`date +"%d/%m/%Y %H:%M:%S"`] ${MYSQL_HOST} [info] Create new full snapshot"
   NEXT_SNAPSHOT_DIR=${FULL_SNAPSHOT_DIR}/`date +%F_%H-%M-%S`
   mkdir -p ${NEXT_SNAPSHOT_DIR}
 
-  # Create a new full backup
+  # Start next full snapshot with mariabackup agent
   ${MARIABACKUP} \
     --backup ${USEROPTIONS} ${ARGS} \
     --extra-lsndir=${NEXT_SNAPSHOT_DIR} \
@@ -114,10 +128,10 @@ fi
 
 MINS=$((${FULL_SNAPSHOT_CYCLE} * (${SNAPSHOT_TTL} + 1 ) / 60))
 echo -e "[`date +"%d/%m/%Y %H:%M:%S"`] ${MYSQL_HOST} [info] Cleaning backup older than ${MINS} minute(s)"
-# Delete old backups
+# Purge all expired snapshot cycles
 for DEL in `find ${FULL_SNAPSHOT_DIR} -mindepth 1 -maxdepth 1 -type d -mmin +${MINS} -printf "%P\n"`
 do
-  echo -e "[`date +"%d/%m/%Y %H:%M:%S"`] ${MYSQL_HOST} [info] Purged backup '$DEL'"
+  echo -e "[`date +"%d/%m/%Y %H:%M:%S"`] ${MYSQL_HOST} [info] Purged backup '${DEL}'"
   rm -rf ${FULL_SNAPSHOT_DIR}/${DEL}
   rm -rf ${INCR_SNAPSHOT_DIR}/${DEL}
 done
